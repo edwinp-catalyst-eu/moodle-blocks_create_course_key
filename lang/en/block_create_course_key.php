8a0eb75e5cf701b7e0133c8b0216179a1e20d64c
Edwin Phillips <edwin.phillips@catalyst-eu.net>
2015-09-17 12:55:57 +0100

    Moodle 2.2 compatibility updates

--- lang/en/block_create_course_key.php
+++ lang/en/block_create_course_key.php
@@ -11,4 +11,8 @@ $string['create_course_key:coursekeyswebservice'] ='Coursekey: Access the course
 
 $string['mail_subjectline'] ='Turteori.dk / End of access to the course: \$a->coursename';
 $string['mail_body'] ='Hi {$a->username},\n\nYour access to the course: {$a->coursename}, at www.turteori.dk has ended.\n\nIf you need to gain access again, you can purchase a coursekey in our webshop at www.turforlag.dk.\n\n\nBest regards\n\n\nTurteori support\nwww.turteori.dk';
+
+// Additional
+$string['blockname'] = 'Create course key';
+$string['pluginname'] = 'Create course key';
 ?>
